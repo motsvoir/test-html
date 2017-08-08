52a7f40439dddda7e7d80b44f999609e84b69572
motsvoir <motsvoir@free.fr>
2017-08-08 23:15:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function ecrire(){
+	var texte = "12/12";
+	document.getElementById("dateDuJour").innerHTML="12/12"; //remplace le bt
+	//alert ("fin");
+}
+
+function demander(){
+	alert ("ok");
+}
